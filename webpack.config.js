2e8a04794a37df578ae05edc5f1ff8f7834d6081
Avinash <123456789+avinash4930c@gmail.com>
2023-09-26 23:53:43 +0530

    Basic Website completed

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,  // Apply style-loader for .css files
+        use: ['style-loader', 'css-loader'], // Use 'style-loader' and 'css-loader'
+      },
+    ],
+  },
 };
